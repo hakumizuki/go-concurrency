264e5dd1158496c6869e7a3da85ad27969e3cdb7
Taichi Masuyama <montanha.masu536@gmail.com>
2022-07-30 23:55:58 +0000

    Impl wg & test code

--- main.go
+++ main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"sync"
+)
 
+func gr(s string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	fmt.Println(s)
+}
+
+func main() {
+	ss := []string{
+		"A",
+		"B",
+		"C",
+	}
+
+	wg := &sync.WaitGroup{}
+	wg.Add(len(ss))
+
+	for i, s := range ss {
+		go gr(fmt.Sprintf("%d-%s", i, s), wg)
+	}
+
+	wg.Wait()
+
+	println("Finished ...")
 }
---
+++ main_w_test.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"io"
+	"os"
+	"strings"
+	"sync"
+	"testing"
+)
+
+func Test_gr(t *testing.T) {
+	stdOut := os.Stdout
+
+	// Get reader and writer
+	r, w, _ := os.Pipe()
+	os.Stdout = w
+
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+
+	go gr("A", wg)
+
+	wg.Wait()
+
+	_ = w.Close()
+
+	result, err := io.ReadAll(r)
+	if err != nil {
+		panic("Panic!")
+	}
+	output := string(result)
+
+	os.Stdout = stdOut
+
+	if !strings.Contains(output, "A") {
+		t.Error("Output does not contain A")
+	}
+}
